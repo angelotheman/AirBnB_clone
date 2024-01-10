f2d0b397e520b19c20e346b21dce68925002609f
Kwabena Twumasi <ktwum721@gmail.com>
2024-01-10 08:35:23 +0000

    Base model file updated

--- models/base_model.py
+++ models/base_model.py
@@ -25,9 +25,9 @@ class BaseModel:
         """
         Initialiazes the id and datetimes of the new instances created
         """
-        id = str(uuid.uuid4())
-        created_at = datetime.now()
-        updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the object class"""
@@ -45,13 +45,10 @@ class BaseModel:
         Returns a dictionary containing key/value of __dict__ for an instance
         """
 
-        obj_dict = {}
+        obj_dict = self.__dict__.copy()
 
-        for key, value in self.__dict__.items():
-            obj_dict[key] = value
-
-        setattr(obj_dict, '__class__', self.__class__.__name__)
-        setattr(obj_dict, 'created_at', self.created_at.isoformat())
-        setattr(obj_dict, 'updated_at', self.updated_at.isoformat())
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
 
         return obj_dict
