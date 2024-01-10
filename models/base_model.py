22404e5e670e81c450217f591c29dcf428eb8804
Kwabena Twumasi <ktwum721@gmail.com>
2024-01-10 08:21:32 +0000

    BaseModel added with TASK 3

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,57 @@
 #!/usr/bin/python3
 """
-Model for the Base
+Module for the Base Class
+- Public instance attributes
+    - id
+    - created_at
+    - updated_at
+- Public instance methods
+    - save(self) - updates the updated_at attribute
+    - to_dict(self) - returns dictionary
+- __str__ method to print
 """
 
 import uuid
-import datetime
+from datetime import datetime
+
 
 class BaseModel:
     """
     This is the base model class
+    It is an abstract class from which all other classes would inherit from
     """
+
+    def __init__(self):
+        """
+        Initialiazes the id and datetimes of the new instances created
+        """
+        id = str(uuid.uuid4())
+        created_at = datetime.now()
+        updated_at = datetime.now()
+
+    def __str__(self):
+        """Returns a string representation of the object class"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        """
+        Updates the public instance attribute update_at with the current datetime
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary containing key/value of __dict__ for an instance
+        """
+
+        obj_dict = {}
+
+        for key, value in self.__dict__.items():
+            obj_dict[key] = value
+
+        setattr(obj_dict, '__class__', self.__class__.__name__)
+        setattr(obj_dict, 'created_at', self.created_at.isoformat())
+        setattr(obj_dict, 'updated_at', self.updated_at.isoformat())
+
+        return obj_dict
