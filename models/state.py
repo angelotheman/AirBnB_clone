0d9e5d17f952d1d53f62fc348e55195d5d7d91ad
Kwabena Twumasi <ktwum721@gmail.com>
2024-01-12 16:36:26 +0000

    State class added

---
+++ models/state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module for the State class
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    Class to manage the State
+    - Attributes
+        - name: (str)
+    """
+
+    name = ""
