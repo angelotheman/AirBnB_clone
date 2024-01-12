97b63a339335b5b166fa9757e0794892f1f54823
Kwabena Twumasi <ktwum721@gmail.com>
2024-01-12 16:35:34 +0000

    Amenity class added

---
+++ models/amenity.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module for the amenity class
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Class to take care of the various amenities
+    - Attributes
+        - name: (str)
+    """
+
+    name = ""
