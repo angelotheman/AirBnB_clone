bd8a5edf829675ec52c42f7e3d61e723ace080d9
Val-Ster <vster310@gmail.com>
2024-01-13 12:13:45 +0000

    to be updated

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Tests for state class
+"""
+import unittest
+from models.state import state
+
+class TestState(unittest.TestCase):
+	def Test_state_values(self):
+		"""
+		Test for when an instance is created
+		"""
+		
+		user = User()
+		
+		self.assertEqual(User.name, "")
+
+if __name__ == '__main__':
+	unittest.main()
