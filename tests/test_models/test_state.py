c60e8a5ae8fa0873b5be2b4850d625c39f74ae8f
Val-Ster <vster310@gmail.com>
2024-01-14 11:02:43 +0000

    fully updated

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -6,14 +6,14 @@ import unittest
 from models.state import State
 
 class TestState(unittest.TestCase):
-	def Test_state_values(self):
+	def Test_state_attributes(self):
 		"""
 		Test for when an instance is created
 		"""
 		
-		user = User()
-		
-		self.assertEqual(User.name, "")
+		state = State()
+		self.assertTrue(hasattr(state, 'name'))
+		self.assertEqual(state.name, "")
 
 	if __name__ == '__main__':
 		unittest.main()
