923820c75562616ad8ca2ffe1a638dc46e0e8e8a
Kwabena Twumasi <ktwum721@gmail.com>
2024-01-15 00:37:16 +0000

    changes made

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,37 +1,67 @@
 #!/usr/bin/python3
 """
-Tests for the place class
+Module for testing the place
 """
 import unittest
 from models.place import Place
 
+
 class TestPlace(unittest.TestCase):
-	
-	def test_place_values(self):
-		place = Place()
-		self.assertTrue(hasattr(place, 'city_id'))
-		self.assertTrue(hasattr(place, 'user_id'))
-		self.assertTrue(hasattr(place, 'name'))
-		self.assertTrue(hasattr(place, 'description'))
-		self.assertTrue(hasattr(place, 'number_rooms'))
-		self.assertTrue(hasattr(place, 'number_bathrooms'))
-		self.assertTrue(hasattr(place, 'max_guest'))
-		self.assertTrue(hasattr(place, 'price_by_night'))
-		self.assertTrue(hasattr(place, 'latitude'))
-		self.assertTrue(hasattr(place, 'longitude'))
-		self.assertTrue(hasattr(place, 'amenity_ids'))
-		
-		self.assertEqual(place.city.id, "")
-		self.assertEqual(place.user.id, "")
-		self.assertEqual(place.name, "")
-		self.assertEqual(place.description, "")
-		self.assertEqual(place.number_rooms, 0)
-		self.assertEqual(place.number_bathrooms, 0)
-		self.assertEqual(place.max_guest, 0)
-		self.assertEqual(place.price_by_night, 0)
-		self.assertEqual(place.latitude, 0.0)
-		self.assertEqual(place.longitude, 0.0)
-		self.assertEqual(place.amenity_ids, [])
-	
+    """
+    Class method to test all Place attributes
+    """
+
+    def setUp(self):
+        """
+        Initializes my object instance
+        """
+        place = Place()
+
+    def test_attributes(self):
+        self.assertTrue(hasattr(place, 'city_id'))
+        self.assertTrue(hasattr(place, 'user_id'))
+        self.assertTrue(hasattr(place, 'name'))
+        self.assertTrue(hasattr(place, 'description'))
+        self.assertTrue(hasattr(place, 'number_rooms'))
+        self.assertTrue(hasattr(place, 'number_bathrooms'))
+        self.assertTrue(hasattr(place, 'max_guest'))
+        self.assertTrue(hasattr(place, 'price_by_night'))
+        self.assertTrue(hasattr(place, 'latitude'))
+        self.assertTrue(hasattr(place, 'longitude'))
+        self.assertTrue(hasattr(place, 'amenity_ids'))
+
+    def test_default_values(self):
+        """
+        Tests the default values of all the attributes
+        """
+        self.assertEqual(place.city.id, "")
+        self.assertEqual(place.user.id, "")
+        self.assertEqual(place.name, "")
+        self.assertEqual(place.description, "")
+        self.assertEqual(place.number_rooms, 0)
+        self.assertEqual(place.number_bathrooms, 0)
+        self.assertEqual(place.max_guest, 0)
+        self.assertEqual(place.price_by_night, 0)
+        self.assertEqual(place.latitude, 0.0)
+        self.assertEqual(place.longitude, 0.0)
+        self.assertEqual(place.amenity_ids, [])
+
+    def test_attribute_instance(self):
+        """
+        Tests the various types of the attributes
+        """
+        self.assertIsInstance(place.city_id, str)
+        self.assertIsInstance(place.user_id, str)
+        self.assertIsInstance(place.name, str)
+        self.assertIsInstance(place.description, str)
+        self.assertIsInstance(place.number_rooms, int)
+        self.assertIsInstance(place.number_bathrooms, int)
+        self.assertIsInstance(place.max_guest, int)
+        self.assertIsInstance(place.price_by_night, int)
+        self.assertIsInstance(place.latitude, float)
+        self.assertIsInstance(place.longitude, float)
+        self.assertIsInstance(place.amenity_ids, list)
+
+
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
