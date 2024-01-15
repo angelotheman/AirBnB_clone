c5b9cd70412e497aff5c6f45caa31f8aa1f6b4dd
Kwabena Twumasi <ktwum721@gmail.com>
2024-01-15 00:35:09 +0000

    test review added

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,24 +1,46 @@
 #!/usr/bin/python3
-
 """
-Test for the review class
+Module for testing the review class
 """
 import unittest
 from models.review import Review
 
-class TestReview(unittest.TestCase):
-	
-	def test_default_values(self):
-		review = Review()
-		self.assertTrue(hasattr(review, 'place_id'))
-		self.assertTrue(hasattr(review, 'user_id'))
-		self.assertTrue(hasattr(review, 'text'))
-		
-		
-		self.assertEqual(review.place_id, "")
-		self.assertEqual(review.user_id, "")
-		self.assertEqual(review.text, "")
 
-	
+class TestReview(unittest.TestCase):
+    """
+    This tests for the Review class methods
+    """
+
+    def setUp(self):
+        """
+        Initialiazation of the class for all functions
+        """
+        review = Review()
+
+    def test_attributes(self):
+        """
+        Testing the various attributes of the class
+        """
+        self.assertTrue(hasattr(review, 'place_id'))
+        self.assertTrue(hasattr(review, 'user_id'))
+        self.assertTrue(hasattr(review, 'text'))
+
+    def test_default_values(self):
+        """
+        Tests default values when the class is instantiated
+        """
+        self.assertEqual(review.place_id, "")
+        self.assertEqual(review.user_id, "")
+        self.assertEqual(review.text, "")
+
+    def test_attribute_instances(self):
+        """
+        Tests the various class types of all the attributes
+        """
+        self.assertIsInstance(review.place_id, str)
+        self.assertIsInstance(review.user_id, str)
+        self.assertIsInstance(review.text, str)
+
+
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
